bb8efa45e3bf19d97215829b0bd696c432c506e3
Malachite <1306515152@qq.com>
2020-03-26 09:04:18 +0800

    multiStringUI complete

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,11 @@ int main()
 	testBaseUI.drawOnBase(testStructUI.draw());
 	stringUI testStringUI = stringUI(0, 0, "Hello, World!");
 	testBaseUI.drawOnBase(testStringUI.draw());
+	multiStringUI testMultiStringUI = multiStringUI(10, 50, 4,  stringUI(0, 0, "This is a multi-line stringUI"),
+																stringUI(0, 0, "This is Line 1"), 
+																stringUI(0, 0, "This is Line 2"), 
+																stringUI(0, 0, "This is Line 3"));
+	testBaseUI.drawOnBase(testMultiStringUI.draw());
 	testBaseUI.drawToScreen();
 	return 0;
 }
